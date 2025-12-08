33c957bcd14c7cf92c30ef4d4dbdbfae4a42ff27
vilarj <lamaravillasan@outlook.com>
2025-12-08 13:45:30 -0500

    Adding the actual ask for each problem

--- Greatest Common Divisor of Strings/CommonDivisor.cs
+++ Greatest Common Divisor of Strings/CommonDivisor.cs
@@ -1,4 +1,35 @@
-﻿namespace Problems.Greatest_Common_Divisor_of_Strings
+﻿/*
+For two strings s and t, we say "t divides s" if and only if 
+s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+
+Output: "ABC"
+
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+
+Output: "AB"
+
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+
+Output: ""
+
+Example 4:
+
+Input: str1 = "AAAAAB", str2 = "AAA"
+
+Output: ""
+*/
+
+namespace Problems.Greatest_Common_Divisor_of_Strings
 {
     public class CommonDivisor
     {

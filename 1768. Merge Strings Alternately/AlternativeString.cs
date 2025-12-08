d20616aaff4f15e1a06417675f4769afc35e4a1c
vilarj <lamaravillasan@outlook.com>
2025-12-08 13:43:28 -0500

    Add project files.

---
+++ 1768. Merge Strings Alternately/AlternativeString.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Problems._1768._Merge_Strings_Alternately
+{
+    public class AlternativeString
+    {
+        public AlternativeString (string w1, string w2)
+        {
+            Console.WriteLine("Word 1: " + w1);
+            Console.WriteLine("Word 2: " + w2);
+            Console.WriteLine("Merged: " + MergeAlternately(w1, w2));
+        }
+
+        public string MergeAlternately(string word1, string word2)
+        {
+            var output = new StringBuilder();
+            int i = 0, j = 0, w1Length = word1.Length, w2Length = word2.Length;
+
+            while (i < w1Length || j < w2Length)
+            {
+                if (i < w1Length)
+                    output.Append(word1[i++]);
+                if (j < w2Length)
+                    output.Append(word2[j++]);
+            }
+
+            return output.ToString();
+        }
+    }
+}
---
+++ Greatest Common Divisor of Strings/CommonDivisor.cs
@@ -0,0 +1,30 @@
+﻿namespace Problems.Greatest_Common_Divisor_of_Strings
+{
+    public class CommonDivisor
+    {
+        public CommonDivisor(string str1, string str2)
+        {
+            Console.WriteLine("String 1: " + str1);
+            Console.WriteLine("String 2: " + str2);
+            Console.WriteLine("GCD of Strings: " + GcdOfStrings(str1, str2));
+        }
+
+        public static string GcdOfStrings(string str1, string str2)
+        {
+            if (str1 + str2 != str2 + str1) return "";
+            int gcdLength = GCD(str1.Length, str2.Length);
+            return str1.Substring(0, gcdLength);
+        }
+
+        private static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}

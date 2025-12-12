64415317db188c45b6c22a138acc72017ce65531
vilarj <lamaravillasan@outlook.com>
2025-12-12 09:58:41 -0500

    Renaming the folder of the problems to a more genetic naming convention | Adding problem 3 and 4

---
+++ Problem 3/Candies.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Problem_3
+{
+    public class Candies
+    {
+        public Candies(int[] candies, int extraCandies) {
+            Console.WriteLine("Candies: [" + string.Join(", ", candies) + "]");
+            Console.WriteLine("Extra Candies: " + extraCandies);
+            Console.WriteLine("Kids with greatest candies: [" + string.Join(", ", KidsWithGreatestCandies(candies, extraCandies)) + "]");
+        }
+
+        public IList<bool> KidsWithGreatestCandies(int[] candies, int extraCandies)
+        {
+            int candiesLength = candies.Length, maxCandies = 0;
+            var kidsGreatestCandies = new List<bool>(candiesLength);
+
+            for (int i = 0; i < candiesLength; i++)
+            {
+                if (candies[i] > maxCandies)
+                {
+                    maxCandies = candies[i];
+                }
+            }
+
+            for (int i = 0; i < candiesLength; i++)
+            {
+                int possible = candies[i] + extraCandies;
+
+                if (possible >= maxCandies)
+                {
+                    kidsGreatestCandies.Add(true);
+                }
+                else
+                {
+                    kidsGreatestCandies.Add(false);
+                }
+            }
+
+            return kidsGreatestCandies;
+        }
+    }
+}
---
+++ Problem 4/CanPlaceFlower.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Problem_4
+{
+    public class CanPlaceFlower
+    {
+        public CanPlaceFlower(int[] flowerbed, int n)
+        {
+            Console.WriteLine("Flowerbed: [" + string.Join(", ", flowerbed) + "]");
+            Console.WriteLine("Number of flowers to place: " + n);
+            Console.WriteLine("Can place flowers: " + CanPlaceFlowers(flowerbed, n));
+        }
+
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            int bedLength = flowerbed.Length;
+
+            for (int i = 0; i < bedLength; i++)
+            {
+                if (flowerbed[i] == 0)
+                {
+                    bool leftEmpty = (i == 0) || (flowerbed[i - 1] == 0);
+                    bool rightEmpty = (i == bedLength - 1) || (flowerbed[i + 1] == 0);
+
+                    if (leftEmpty && rightEmpty)
+                    {
+                        flowerbed[i] = 1;
+                        n--;
+
+                        if (n == 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return n <= 0;
+        }
+    }
+}

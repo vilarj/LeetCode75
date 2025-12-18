0f2f458930897b24100aede32b61ed33e1fd10b0
vilarj <lamaravillasan@outlook.com>
2025-12-18 09:35:29 -0500

    Adding problem 7 and 8

---
+++ Problem 7/ProductOfArray.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Problems.Problem_7
+{
+    public class ProductOfArray
+    {
+        public ProductOfArray(int[] nums)
+        {
+            Console.WriteLine(ProductExceptSelf(nums));
+        }
+        private static int[] ProductExceptSelf(int[] nums)
+        {
+            if (nums == null || nums.Length <= 1) return nums;
+            if (nums.Length == 1) return [1];
+
+            int len = nums.Length;
+            int[] answer = new int[len];
+            int leftProduct = 1;
+            int rightProduct = 1;
+
+            for (int i = 0; i < len; i++)
+            {
+                answer[i] = leftProduct;
+                leftProduct *= nums[i];
+            }
+
+            for (int i = len - 1; i >= 0; i--)
+            {
+                answer[i] *= rightProduct;
+                rightProduct *= nums[i];
+            }
+
+            return answer;
+        }
+
+    }
+}
---
+++ Problem 8/IncreasingTriplets.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Problems.Problem_8
+{
+    public class IncreasingTriplets
+    {
+        public IncreasingTriplets(int[] n)
+        {
+            Console.WriteLine(Triplets(n));
+        }
+        private static bool Triplets(int[] nums)
+        {
+            int first = int.MaxValue, second = int.MaxValue;
+
+            foreach (int num in nums)
+            {
+                if (num <= first) first = num; // Update first
+                else if (num <= second) second = num; // Update second
+                else return true; // num > first && num > second
+            }
+            return false;
+        }
+    }
+}

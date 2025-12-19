047da840e55a85908bdc37d1c9bfb42cbbb78995
vilarj <lamaravillasan@outlook.com>
2025-12-19 07:02:09 -0500

    Problem 9

---
+++ Problem 9/StringCompress.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Problems.Problem_9
+{
+    public class StringCompress
+    {
+        public StringCompress(char[] n)
+        {
+            Console.WriteLine(Compress(n));
+        }
+
+        private static int Compress(char[] chars)
+        {
+            int write = 0;
+            int read = 0;
+
+            while (read < chars.Length)
+            {
+                char current = chars[read];
+                int count = 0;
+
+                while (read < chars.Length && chars[read] == current)
+                {
+                    read++;
+                    count++;
+                }
+
+                chars[write++] = current;
+
+                if (count > 1)
+                {
+                    foreach (char c in count.ToString())
+                    {
+                        chars[write++] = c;
+                    }
+                }
+            }
+
+            return write;
+        }
+    }
+}
